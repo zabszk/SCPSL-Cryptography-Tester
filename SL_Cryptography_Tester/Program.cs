1c3960eac9cb3683ba90107b7b2e3fbe814b99c5
Łukasz Jurczyk <admin@zabszk.net>
2018-07-05 16:44:31 +0200

    Add project files.

---
+++ SL_Cryptography_Tester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SL_Cryptography_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
